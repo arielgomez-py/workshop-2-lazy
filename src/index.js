5ce5513045ba27fa17c9005f7ffe4ae7ddb8003d
unknown <arielgomezmendez94@gmail.com>
2022-05-12 09:47:07 -0400

    Add images, lazy loading

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
 
-console.log('Happy hacking :)')
+import { registerImage } from "./lazy";
+
+//crear una imagen
+// agregar imagen
+
+const minimum = 1;
+const maximum = 122;
+//crear funcion random q genere numeros del 1 al 122
+const random = () => Math.floor(Math.random() * (maximum - minimum)) + minimum;
+
+const createImageNode = () => {
+    const container = document.createElement('div');
+    container.className = "p-4";
+
+    const imagen = document.createElement('img');
+    imagen.className = 'mx-auto';
+    imagen.width = '320';
+    imagen.dataset.src = `https://randomfox.ca/images/${random()}.jpg`; // to do
+    container.appendChild(imagen);
+
+    return container;
+};
+
+const mountNode = document.getElementById("images");
+
+const addButton = document.querySelector('button');
+
+const addImage = () => {
+    const newImage = createImageNode();
+    mountNode.append(newImage);
+    registerImage(newImage);
+};   
+
+addButton.addEventListener("click", addImage);
+
+
---
+++ src/lazy.js
@@ -0,0 +1,25 @@
+
+const isIntersecting = (entry) => {
+    return entry.isIntersecting; // true dentro de la pantalla
+};
+
+const loadImage = (entry) => {
+    const container = entry.target;
+    const imagen = container.firstChild;
+    const url = imagen.dataset.src;
+    imagen.src = url;
+    //desregistra la imagen (unlisten)
+    observer.unobserve(container);
+};
+
+// se utieliza la api intersection observer creando una instancia
+const observer = new IntersectionObserver((entries) => {
+    entries
+        .filter(isIntersecting)
+        .forEach(loadImage)
+});
+
+// alucion al intersection observer
+export const registerImage = (imagen) => {
+    observer.observe(imagen);
+}
