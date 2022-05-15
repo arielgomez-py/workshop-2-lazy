865b52f4410972b05f31d1f48bc304ba3e742064
Ariel <arielgomezmendez94@gmail.com>
2022-05-15 13:59:44 -0400

    Remove all images

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 
+import { doc } from "prettier";
 import { registerImage } from "./lazy";
 
 //crear una imagen
 // agregar imagen
+
 const minimum = 1;
 const maximum = 122;
 //crear funcion random q genere numeros del 1 al 122
@@ -10,36 +12,36 @@ const random = () => Math.floor(Math.random() * (maximum - minimum)) + minimum;
 
 const createImageNode = () => {
     const container = document.createElement('div');
-    container.className = "p-4";
-
-    //create a boxNode div, with color grey
-    const boxNode = document.createElement('div');
-    boxNode.className = 'bg-gray-300';
-    boxNode.style.minHeight = '100px';
-    boxNode.style.display = "inline-block";
+    container.className = "container";
+    container.id ="containerID";
 
     const imagen = document.createElement('img');
-    imagen.className = 'mx-auto';
-    imagen.width = '320';
-    imagen.height = '100px';
-    
+    imagen.className = 'imagen';
+    imagen.id = "imagen";
     imagen.dataset.src = `https://randomfox.ca/images/${random()}.jpg`; // to do
-    container.appendChild(boxNode);
-    boxNode.appendChild(imagen);
+    container.appendChild(imagen);
+
     return container;
 };
 
-//button
 const mountNode = document.getElementById("images");
 
-const addButton = document.querySelector('button');
+//Buttons
+const addButton = document.getElementById("Add-Button");
+const cleanButton = document.getElementById("Clean-Button");
 
 const addImage = () => {
     const newImage = createImageNode();
     mountNode.append(newImage);
     registerImage(newImage);
-};   
+};
+
+//remove all images
+const cleanAll =() => {
+    document.getElementById('images').innerHTML = ''; 
+}
 
 addButton.addEventListener("click", addImage);
 
+cleanButton.addEventListener("click", cleanAll);
 
